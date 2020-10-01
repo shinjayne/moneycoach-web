26de25a06950f4cf18fa822b5135469129152dc1
jayneshin <shinjayne@gmail.com>
2020-10-02 01:34:16 +0900

    configure typescript and replace _app.js to _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import {AppProps} from "next/app";
+import * as React from "react";
+
+import '../styles/globals.css'
+
+function MyApp({Component, pageProps}: AppProps) {
+    return <Component {...pageProps} />
+}
+
+export default MyApp
